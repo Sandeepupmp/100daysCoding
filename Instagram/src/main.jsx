956c9c7fecf40fbe138eda1a87c71c5d95645994
sandeep <sandeepupmp@gmail.com>
2023-11-30 23:05:42 +0530

    added reac router dom

--- Instagram/src/main.jsx
+++ Instagram/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import { extendTheme } from '@chakra-ui/react'
 import { mode } from '@chakra-ui/theme-tools'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const config = {
@@ -25,10 +26,10 @@ const theme = extendTheme({ config , styles })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
      <ChakraProvider theme={theme}>
-
     <App />
     </ChakraProvider>
-
+    </BrowserRouter>
   </React.StrictMode>,
 )
